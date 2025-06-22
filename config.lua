70718a66ae00268f8aaa0b87e166e54313d1b15e
DevZzTeam <166993435+DevZzTeam@users.noreply.github.com>
2025-06-23 00:37:56 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,6 +4,7 @@ config.amplificator = 0.5
 config.maxDistanceX = 1 --max distance from the default camera rotation on axe X
 config.maxDistanceY = 1
 config.disableAllControlAction = true
+config.displayCursor = true
 
 config.command = {
     use = true,
